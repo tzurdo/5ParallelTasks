8a0c11f658e607ec04bde28dda43adf7d000ae71
tzurdo <144802384+tzurdo@users.noreply.github.com>
2023-12-07 17:13:07 +0200

    Add files via upload

---
+++ 5ParallelTasks/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+
+namespace _5ParallelTasks
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Finish.");
+
+            ConcurrentQueue<Action> actions = new ConcurrentQueue<Action>();
+
+            for (int i = 0; i < 100; i++)
+            {
+                int index = i;
+
+                Action a = () => Console.WriteLine($"Task with index: {index}");
+                actions.Enqueue(a);
+            }
+
+            TaskRunner taskRunner = new TaskRunner(actions, 5);
+            await taskRunner.RunAsync();
+
+            Console.WriteLine("Finish.");
+        }
+    }
+}
---
+++ 5ParallelTasks/TaskRunner.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _5ParallelTasks;
+
+internal class TaskRunner
+{
+    public TaskRunner(ConcurrentQueue<Action> actions, int maxCount)
+    {
+        _actions = actions;
+        _semaphoreSlim = new SemaphoreSlim(5, maxCount);
+    }
+
+    public async Task RunAsync()
+    {
+        while (_actions.TryPeek(out _))
+        {
+            try
+            {
+                await _semaphoreSlim.WaitAsync();
+                Console.WriteLine($"Semaphore CurrentCount: {_semaphoreSlim.CurrentCount}");
+
+                _actions.TryDequeue(out Action action);
+                await Task.Run(action);
+            }
+            finally
+            {
+                _semaphoreSlim.Release();
+            }
+        }
+    }
+
+    private ConcurrentQueue<Action> _actions = null;
+    private SemaphoreSlim _semaphoreSlim = null;
+}
